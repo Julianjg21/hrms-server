aa6ac6f513d6d0eed4d82ab010cbd6c89f2739b4
Julian Jimenez <julianjimenez2128@gmail.com>
2024-11-28 09:15:48 -0500

    feat(login): add and connect login routes, improve db connection and error handling

--- src/App.mjs
+++ src/App.mjs
@@ -1,23 +1,25 @@
 import express from "express";
 import cors from "cors";
-
-
+import AuthRoutes from "./routes/AuthRoutes.mjs";
+import db from "./config/Database.mjs";
 const App = express();
-
 
 App.use(cors()); //enable CORS
 App.use(express.json()); //json parsing
 
+App.use("/auth", AuthRoutes);
 
-
-
-
-App.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500).json({
-    error: true,
-    message: err.message || "Error interno del servidor",
-  });
+//Database connection management
+(async () => {
+  try {
+    const connection = await db.getConnection();
+    console.log("Connection to the database established!!");
+    connection.release();
+  } catch (err) {
+    console.error("Database connection error:", err);
+  }
+})();
+db.on("error", (err) => {
+  console.error("Database connection error:", err);
 });
-
 export default App;

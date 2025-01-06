f01a3c2c9da5567638925df85dbc07ca6ab43165
Julian Jimenez <julianjimenez2128@gmail.com>
2025-01-06 18:18:59 -0500

    feat(models): add RoleModel for user permissions and enhance UserModel with user registration functionality

---
+++ src/models/RoleModel.mjs
@@ -0,0 +1,17 @@
+import db from "../config/Database.mjs";
+//get permissions of the user
+export const findUserPermission = async (userId) => {
+  const [rows] = await db.query(
+    "SELECT * FROM permissions p INNER JOIN role_permissions rp ON  rp.permission_id = p.id where rp.role_id IN (SELECT role_id from user_roles where user_roles.user_id =  ? )",
+    [userId]
+  );
+  //return response from database
+  return rows;
+};
+
+//get all permissions
+export const getAllPermissions = async () => {
+  const [rows] = await db.query("SELECT * FROM permissions");
+  //return data from database
+  return rows;
+};
--- src/models/UserModel.mjs
+++ src/models/UserModel.mjs
@@ -4,17 +4,63 @@ import db from "../config/Database.mjs";
 export const findUserByEmail = async (email) => {
   //select all data from the users table if the email is found
   const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
-//return response from database
+  //return response from database
   return rows[0];
-  };
+};
 
-  //search the user identification received in the database
+//search the user identification received in the database
 export const findUserByIdentification = async (identification) => {
- //select all data nested to the user ID
+  //select all data nested to the user ID
   const [rows] = await db.query(
     "SELECT * FROM users usr INNER JOIN user_details ud ON usr.user_id = ud.user_id WHERE ud.identification = ?",
     [identification]
   );
   //return response from database
   return rows[0];
+};
+
+export const registerUser = async (userData, userDetails) => {
+  try {
+    const { email, password_hash, user_type } = userData;
+    const {
+      user_names,
+      last_names,
+      phone_number,
+      identification,
+      birth_date,
+      bank,
+      account_number,
+      type_identification
+    } = userDetails;
+
+     //insert new user
+    const [rows] = await db.query(
+      "INSERT INTO users (email, password_hash, user_type) VALUES (?, ?, ?)",
+      [email, password_hash, user_type]
+    );
+
+  //Get the ID of the newly created user
+    const user_id = rows.insertId;
+
+  //Insert new user data
+    await db.query(
+      "INSERT INTO user_details (user_id, user_names, last_names, phone_number, identification, birth_date, bank, account_number, type_identification) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        user_id,
+        user_names,
+        last_names,
+        phone_number,
+        identification,
+        birth_date,
+        bank,
+        account_number,
+        type_identification
+      ]
+    );
+
+    console.log("Usuario y detalles insertados correctamente.");
+  } catch (error) {
+    console.error("Error al registrar el usuario:", error.message);
+    throw error; //Throw the error to handle it out of scope
+  }
 };

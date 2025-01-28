7f4ea8b38119a74af6fd8dbd6d87dc11699d11c6
Julian Jimenez <julianjimenez2128@gmail.com>
2025-01-27 22:26:34 -0500

    feat(controllers): add user management functions and refactor createUsers for clarity

--- src/controllers/UserManagementController.mjs
+++ src/controllers/UserManagementController.mjs
@@ -1,24 +1,30 @@
-import { registerUser } from "../models/UserModel.mjs";
+import {
+  registerUserModel,
+  searchUsersModel,
+  updateUserDataModel,
+  deleteUserModel,
+  getEmployees,
+} from "../models/UserModel.mjs";
 import bcrypt from "bcrypt";
-export const createUsers = async (req, res) => {
+export const createUsersController = async (req, res) => {
   const { userData, userDetails } = req.body;
   try {
     let userType = "";
     //Check the user type
-    switch (userDetails.employeeType) {
+    switch (userDetails.employee_type) {
       case "Bartender":
       case "Mesero":
       case "Host":
       case "Operario de limpieza":
       case "DJ":
       case "Operario de seguridad":
-        userType = "Employee";
+        userType = "employee";
         break;
       case "Administrador":
         userType = "administrator";
         break;
       case "Gerente":
-        userType = "Manager";
+        userType = "manager";
         break;
       default:
         break;
@@ -33,7 +39,7 @@ export const createUsers = async (req, res) => {
       user_type: userType,
     };
     //Register the user
-    await registerUser(newUserData, userDetails);
+    await registerUserModel(newUserData, userDetails);
     return res
       .status(200)
       .json({ message: "usuario registrado correctamente." });
@@ -43,4 +49,79 @@ export const createUsers = async (req, res) => {
       .json({ message: "Error al registrar el usuario", error: error.message });
   }
 };
-export default createUsers;
+
+export const searchUsersController = async (req, res) => {
+  const { user_names, type_identification, identification } = req.body;
+  try {
+    const user = await searchUsersModel(
+      user_names,
+      type_identification,
+      identification
+    );
+    res.status(200).json({ message: "Usuario encontrado", user });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al buscar el usuario",
+      error: error.message,
+    });
+  }
+};
+
+export const editUserDataController = async (req, res) => {
+  const { userData, userDetails } = req.body;
+  let user_type = "";
+  //Check the user type
+  switch (userDetails.employee_type) {
+    case "Bartender":
+    case "Mesero":
+    case "Host":
+    case "Operario de limpieza":
+    case "DJ":
+    case "Operario de seguridad":
+      user_type = "employee";
+      break;
+    case "Administrador":
+      user_type = "administrator";
+      break;
+    case "Gerente":
+      user_type = "manager";
+      break;
+    default:
+      break;
+  }
+  try {
+    await updateUserDataModel(userData, userDetails, user_type);
+    res.status(200).json({ message: "Usuario actualizado correctamente." });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al actualizar el usuario",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteUserController = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await deleteUserModel(id);
+    res.status(200).json({ message: "Usuario eliminado correctamente." });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al eliminar el usuario", error: error.message });
+  }
+};
+
+export const searchEmployeesController = async (req, res) => {
+  const { employee_type } = req.body;
+  try {
+    const user = await getEmployees(employee_type);;
+    res.status(200).json({ message: "Usuarios encontrados", user });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al buscar el usuario",
+      error: error.message,
+    });
+  }
+};
+export default createUsersController;
